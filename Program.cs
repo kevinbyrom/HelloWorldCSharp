4242461cb8f86ef8e7f697638a3d9710b8ca986c
Kevin Byrom <kevinbyrom@Kevins-MBP.home>
2017-08-06 20:39:07 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace HelloWorldCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            SampleLibrary.Utility.Echo("Here we go!!!");
+            var cities = new string[] {"LosAngeles", "NewYork", "SanFrancisco"};
+
+            Parallel.ForEach(cities, c => 
+            { 
+                Thread.Sleep(1000);
+                Console.WriteLine(c);
+            });
+            Console.WriteLine("Done Parallel For");
+
+            foreach (var c in cities)
+            {
+                Task.Run(()=>
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine(c);
+                    });
+            }
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        } 
+    }
+}
